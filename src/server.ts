20c7930f602587dbee6fae0eb29b73c03fa39093
vishalkirtaniya <vishalkirtaniyaofficial@gmail.com>
2026-03-26 16:44:57 +0530

    feat: added api for orderRoutes

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { newArrivalsRoutes } from "./modules/newArrivals/newArrivals.routes";
 import { topSellingRoutes } from "./modules/topSelling/topSelling.routes";
 import { customerReviewRoutes } from "./modules/customerReviews/customerReviews.routes";
 import { cartRoutes } from "./modules/cart/cart.routes";
+import { orderRoutes } from "./modules/orders/order.routes";
 import redis from "./services/redis";
 import pool from "./services/db";
 
@@ -33,9 +34,9 @@ async function bootstrap() {
   fastify.register(topSellingRoutes, { prefix: "/api/top-selling" });
   fastify.register(customerReviewRoutes, { prefix: "/api/reviews" });
   fastify.register(cartRoutes, { prefix: "/api/cart" });
+  fastify.register(orderRoutes, { prefix: "/api/orders" });
 
   // TODO: register remaining modules as they're built:
-  // fastify.register(orderRoutes,           { prefix: '/api/orders' });
   // fastify.register(paymentRoutes,         { prefix: '/api/payments' });
 
   // 3. Health check
